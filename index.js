56e97e872ec707f792f7b03b2ee4ec26957b25da
Nafie Asfour <ph.nafee.asfour@gmail.com>
2023-06-16 19:23:05 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your Circle class here
+class Circle {
+    constructor(radius) {
+      this._radius = radius;
+    }
+  
+    get radius() {
+      return this._radius;
+    }
+  
+    get diameter() {
+      return this._radius * 2;
+    }
+  
+    get circumference() {
+      return 2 * Math.PI * this._radius;
+    }
+  
+    get area() {
+      return Math.PI * this._radius * this._radius;
+    }
+  
+    set diameter(diameter) {
+      this._radius = diameter / 2;
+    }
+  
+    set circumference(circumference) {
+      this._radius = circumference / (2 * Math.PI);
+    }
+  
+    set area(area) {
+      this._radius = Math.sqrt(area / Math.PI);
+    }
+  }
